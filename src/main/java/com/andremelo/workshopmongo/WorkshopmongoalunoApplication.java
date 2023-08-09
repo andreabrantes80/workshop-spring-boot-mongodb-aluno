2678f70dea2459e79b80bb851f65c76bcf4df5f0
andreabrantes80 <meloabrantes@gmail.com>
2023-08-09 10:39:44 -0300

    Primeiro Commit - projeto criado

---
+++ src/main/java/com/andremelo/workshopmongo/WorkshopmongoalunoApplication.java
@@ -0,0 +1,13 @@
+package com.andremelo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoalunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoalunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andremelo/workshopmongo/WorkshopmongoalunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremelo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoalunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
